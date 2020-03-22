070ed21ded46d3e1c7b728ce238fb55d4a8ce27b
maderalukaszQA <@nulka171244>
2020-03-22 14:55:42 +0100

    added tests for READ

--- src/test/java/jsonplaceholderCRUDDemo/JsonplaceholderGetTwoTest.java
+++ src/test/java/jsonplaceholderCRUDDemo/JsonplaceholderGetTwoTest.java
@@ -9,7 +9,7 @@ import io.restassured.response.Response;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-public class JsonplaceholderGetTest {
+public class JsonplaceholderGetTwoTest {
 
     //GIVEN - konfiguracja
     //WHEN - wysłanie requesta
